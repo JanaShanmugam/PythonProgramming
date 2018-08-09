52735d85df96d26a6d4f3cf6a7386017bf8fb6cb
JanaShanmugam <36722610+JanaShanmugam@users.noreply.github.com>
2018-08-09 16:24:35 +0530

    Create sort.py

---
+++ Beginnerlevel/sort.py
@@ -0,0 +1,12 @@
+num_array = list()
+t=0
+num =int(input("Enter no of elements :"))
+print ('Enter numbers : ')
+for i in range(int(num)):
+    n = int(input("num :"))
+    num_array.append(int(n))
+print ('ARRAY: ', num_array)
+num_array.sort()
+print(num_array)  
+      
+    
