08bb3952b3636e57b8c995bd2880ea95380be4a5
JanaShanmugam <36722610+JanaShanmugam@users.noreply.github.com>
2018-08-09 16:34:11 +0530

    Create arrvalwithindex.py

---
+++ Beginnerlevel/arrvalwithindex.py
@@ -0,0 +1,11 @@
+num_array = list()
+t=0
+num =int(input("Enter no of elements :"))
+print ('Enter numbers : ')
+for i in range(int(num)):
+    n = int(input("num :"))
+    num_array.append(int(n))
+print ('ARRAY: ', num_array)
+for i in range(int(num)) :
+   print(num_array[i],i)
+      
